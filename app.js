ff2f9d4e827b75ad00ca9d6f2a1253eb0fd91c22
kylearora <kylearora@gmail.com>
2017-09-14 18:51:15 -0500

    create routes; rendered test index page; created tables and relations between tables with foreign keys

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express")
+const app = express()
+const bodyParser = require("body-parser")
+const mustache = require("mustache-express")
+app.engine('mustache', mustache())
+app.set('view engine', 'mustache')
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+const mainRoute = require("./routes/mainRoute")
+app.use(mainRoute)
+
+app.listen(3000, function(req, res){
+  console.log("Start gabbing FOOL");
+})
